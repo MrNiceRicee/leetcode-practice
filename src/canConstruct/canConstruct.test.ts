2f75a55fae2afa1f422168552318a820c3ecceea
Joshua Santos <JoshCSan@gmail.com>
2023-09-11 09:13:43 -0700

    - add: canConstruct

--- src/canConstruct/canConstruct.test.ts
+++ src/canConstruct/canConstruct.test.ts
@@ -8,4 +8,23 @@ describe("canConstruct", () => {
     const results = canConstruct(ransomNote, magazine);
     expect(results).toBe(true);
   });
+  const testTable = [
+    { ransomNote: "aa", magazine: "aab", results: true },
+    { ransomNote: "aa", magazine: "ab", results: false },
+    { ransomNote: "aa", magazine: "aab", results: true },
+    { ransomNote: "aa", magazine: "ab", results: false },
+    { ransomNote: "aa", magazine: "aab", results: true },
+    { ransomNote: "aa", magazine: "ab", results: false },
+    { ransomNote: "aa", magazine: "aab", results: true },
+    { ransomNote: "aa", magazine: "ab", results: false },
+    { ransomNote: "aa", magazine: "aab", results: true },
+    { ransomNote: "aa", magazine: "ab", results: false },
+  ];
+
+  for (const { ransomNote, magazine, results } of testTable) {
+    test(`input: ${ransomNote}, ${magazine}`, () => {
+      const res = canConstruct(ransomNote, magazine);
+      expect(res).toBe(results);
+    });
+  }
 });
--- src/canConstruct/canConstruct.ts
+++ src/canConstruct/canConstruct.ts
@@ -1,15 +1,25 @@
-export function canConstruct(ransomNote: string, magazine: string): boolean {
-  if (!ransomNote) return false;
-  let isPossible = true;
-  for (let i = 0; i < ransomNote.length || !isPossible; i++) {
-      isPossible = magazine.includes(ransomNote[i]);
-  }
-  return isPossible;
-};
+/*
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
 
-function test() {
-  const ransomNote = "aa";
-  const magazine = "aab";
-  const results = canConstruct(ransomNote, magazine);
-  console.log(results);
+Each letter in magazine can only be used once in ransomNote.
+
+
+*/
+
+export function canConstruct(ransomNote: string, magazine: string): boolean {
+  if (!ransomNote || !magazine) return false;
+  if (ransomNote.length > magazine.length) return false;
+
+  // We iterate through ransomNote and check if each character is contained in magazineChars.
+  // If it is, we remove the character from magazineChars.
+  // If it isn't, we return false.
+
+  const magazineChars = magazine.split("");
+  for (let i = 0; i < ransomNote.length; i++) {
+    const index = magazineChars.indexOf(ransomNote[i]);
+    if (index === -1) return false;
+    magazineChars.splice(index, 1);
+  }
+
+  return true;
 }
